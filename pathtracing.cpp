fc7404129ab3deb8c2c20382e390187786697b81
Kazuki Miyazaki <kazuki.saxophone@gmail.com>
2017-09-26 15:08:22 +0900

    original pathtracing

--- pathtracing.cpp
+++ pathtracing.cpp
@@ -1,11 +1,8 @@
-
-// 1. マクロを定義．
 #define EIGEN_NO_DEBUG // コード内のassertを無効化．
 #define EIGEN_DONT_PARALLELIZE // 並列を無効化．
 #define EIGEN_MPL2_ONLY // LGPLライセンスのコードを使わない．
 #define PI 3.141592653589793;
 
-// 2. Eigenをインクルード．
 #include <iostream>
 #include <random>
 #include <opencv2/core/core.hpp>
@@ -13,7 +10,7 @@
 #include <Eigen/Core>
 #include <Eigen/Geometry>
 using namespace Eigen;
-const int N = 1000;
+const int N = 500;
 
 struct line{
   Vector3d org;
@@ -46,7 +43,7 @@ double BSDF(Vector3d x, Vector3d wi, Vector3d wo);
 
 int main(int argc, char const *argv[]) {
   /* code */
-  MatrixXd input = MatrixXd::Zero(50,50);
+  MatrixXd input = MatrixXd::Zero(100,100);
   struct plane eye, lens, light;
   eye.point << 5.0, 0.0, 0.0;
   eye.area << 0.1, 0.1;
@@ -58,15 +55,13 @@ int main(int argc, char const *argv[]) {
   light.area << 1.0, 1.0;
   light.normal = -light.point.normalized();
   input = Pathtracing(input, eye, lens, light);
-  std::cout << "input\n" << input << std::endl;
-
-
+  //std::cout << "input\n" << input << std::endl;
 
   double* data = new double[input.cols()*input.rows()];
   cv::Mat img(input.cols(), input.rows(), CV_64FC1, data);
   for (int i = 0; i < input.rows(); i++) {
     for (int j = 0; j < input.cols(); j++) {
-      data[i * input.cols() + j] = 1000 * input(i,j);
+      data[i * input.cols() + j] = input(i,j);
     }
   }
   //std::cout << "img\n" << img << std::endl;
@@ -100,6 +95,7 @@ MatrixXd Pathtracing(
   horizontal.normal << 0.0, 1.0, 0.0;
   horizontal.area << 1.0, 1.0;
   struct sphere unitsphere = { Vector3d::Zero(), 1.0 };
+  double A, B, C, D;
 
   Vector3d raynormal, wo, wi;
 
@@ -122,15 +118,32 @@ MatrixXd Pathtracing(
         ray.org = x0;
         ray.dir = (x0 - xp).normalized();
         double psigma = 1.0 / 2.0 * PI;
-        double alpha = ray.dir.dot(eye.normal) / pax0 * psigma;/*重みづけ未考慮*/
+        double alpha = 1.0;//ray.dir.dot(eye.normal) / pax0 * psigma;/*重みづけ未考慮*/
         //std::cout << "start" << std::endl;
 
 
         do{//ray をトレースして衝突がある
           tplane = (horizontal.point - ray.org).dot(horizontal.normal) / ray.dir.dot(horizontal.normal);
+          //if ((ray.org + tplane * ray.dir - horizontal.point).norm() > 5.0) {
+          //  tplane = -1.0;
+          //}
           tlight = (light.point - ray.org).dot(light.normal) / ray.dir.dot(light.normal);
-          tsphere = (-(ray.org - unitsphere.center).dot(ray.dir) - ray.dir.norm() * unitsphere.radius) / ray.dir.dot(ray.dir);
+          if ((ray.org + tlight * ray.dir - light.point).norm() > 10.0) {
+            tlight = -1.0;
+          }
 
+          A = pow(ray.dir.norm(), 2);
+          B = ray.dir.dot(ray.org - unitsphere.center);
+          C = pow((ray.org - unitsphere.center).norm(), 2) - pow(unitsphere.radius, 2);
+          D = B * B - A * C;
+
+          if (D < 0) {
+            tsphere = -1.0;
+          }
+          else {
+            tsphere = (-B - sqrt(D)) / A;
+          }
+          //std::cout << tsphere << std::endl;
 
           tplane = 1.0 / tplane;
           tlight = 1.0 / tlight;
@@ -179,8 +192,8 @@ MatrixXd Pathtracing(
             break;
           }
           wi << point(mt), point(mt), point(mt);
-          while (wi.dot(raynormal) < 0) {
-            wi << point(mt), point(mt), point(mt);
+          if (wi.dot(raynormal) < 0) {
+            wi = -wi;
           }
           wi = wi / wi.norm();
 
@@ -201,6 +214,7 @@ MatrixXd Pathtracing(
        } while (t > 0.0);
       }
     }
+    std::cout << "loading..." << (double)i / (double)N * 100.0 << "%" << std::endl;
   }
 
   return input / (double)N;
